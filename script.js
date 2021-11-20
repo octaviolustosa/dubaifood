98e86c658a20cfeac23de188d0dd94d201ae6aef
octaviolustosa <94326968+octaviolustosa@users.noreply.github.com>
2021-11-20 12:20:46 -0300

    Input box info

--- script.js
+++ script.js
@@ -23,18 +23,49 @@
 // window.addEventListener("scroll", fdleft);
 
 
+
+// box 1
 showBoxOne = window.document.getElementById("box-1");
 
 var boxOne = function() {
     var y = window.scrollY;
-    if (y >= 700) {
+    if (y >= 450) {
         showBoxOne.className = "box-1 show"
     } else {
         showBoxOne.className = "box-1 hide"
     }
 };
-
 window.addEventListener("scroll", boxOne);
+
+
+
+// box 2
+showBoxTwo = window.document.getElementById("box-2");
+
+var boxTwo = function() {
+    var y = window.scrollY;
+    if (y >= 575) {
+        showBoxTwo.className = "box-2 show-left"
+    } else {
+        showBoxTwo.className = "box-2 hide-left"
+    }
+};
+window.addEventListener("scroll", boxTwo);
+
+
+
+// box 3
+showBoxThree = window.document.getElementById("box-3");
+
+var boxThree = function() {
+    var y = window.scrollY;
+    if (y >= 700) {
+        showBoxThree.className = "box-3 show"
+    } else {
+        showBoxThree.className = "box-3 hide"
+    }
+};
+window.addEventListener("scroll", boxThree);
 
 
 
