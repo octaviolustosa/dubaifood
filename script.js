92afe431cdec4268f231930ed80d2e4753963815
octaviolustosa <94326968+octaviolustosa@users.noreply.github.com>
2021-11-24 14:52:59 -0300

    Responsive Issues

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ showBoxOne = window.document.getElementById("box-1");
 
 var boxOne = function() {
     var y = window.scrollY;
-    if (y >= 450) {
+    if (y >= 350) {
         showBoxOne.className = "box-1 show"
     } else {
         showBoxOne.className = "box-1 hide"
@@ -44,7 +44,7 @@ showBoxTwo = window.document.getElementById("box-2");
 
 var boxTwo = function() {
     var y = window.scrollY;
-    if (y >= 575) {
+    if (y >= 550) {
         showBoxTwo.className = "box-2 show-left"
     } else {
         showBoxTwo.className = "box-2 hide-left"
@@ -59,7 +59,7 @@ showBoxThree = window.document.getElementById("box-3");
 
 var boxThree = function() {
     var y = window.scrollY;
-    if (y >= 700) {
+    if (y >= 650) {
         showBoxThree.className = "box-3 show"
     } else {
         showBoxThree.className = "box-3 hide"
@@ -68,6 +68,32 @@ var boxThree = function() {
 window.addEventListener("scroll", boxThree);
 
 
+
+// skills section
+
+// headline h-box
+hBoxInfo = document.getElementById('h-box-info');
+var hShow = function() {
+    var y = window.scrollY;
+    if (y >= 2500) {
+        hBoxInfo.className = 'h-box-info show'
+    } else {
+        hBoxInfo.className = 'h-box-info hideLeft'
+    }
+};
+window.addEventListener("scroll", hShow);
+
+// h-box 
+hBox = document.getElementById('h-box');
+var hShow = function() {
+    var y = window.scrollY;
+    if (y >= 2350) {
+        hBox.className = 'h-box show'
+    } else {
+        hBox.className = 'h-box h-hide'
+    }
+};
+window.addEventListener("scroll", hShow);
 
 
 
