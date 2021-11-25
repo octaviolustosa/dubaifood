6fb68c999113d673f7ff13f51c918fed03450f40
octaviolustosa <94326968+octaviolustosa@users.noreply.github.com>
2021-11-25 16:06:31 -0300

    Fixing Issues

--- script.js
+++ script.js
@@ -1,32 +1,5 @@
-// fade sec 1
-// showContent = document.getElementById('box-1');
-// var fade = function() {
-//     var y = window.scrollY;
-//     if (y >= 750) {
-//         showContent.className = 'box-1 show'
-//     } else {
-//         showContent.className = 'box-1 hide'
-//     }
-// };
-
-// window.addEventListener("scroll", fade);
-
-// fadeleft = document.getElementById('info-sec-1');
-// var fdleft = function() {
-//     var y = window.scrollY;
-//     if (y >= 300) {
-//         fadeleft.className = 'info-sec-1 show'
-//     } else {
-//         fadeleft.className = 'info-sec-1 hideLeft'
-//     }
-// };
-// window.addEventListener("scroll", fdleft);
-
-
-
 // box 1
 showBoxOne = window.document.getElementById("box-1");
-
 var boxOne = function() {
     var y = window.scrollY;
     if (y >= 350) {
@@ -37,11 +10,8 @@ var boxOne = function() {
 };
 window.addEventListener("scroll", boxOne);
 
-
-
 // box 2
 showBoxTwo = window.document.getElementById("box-2");
-
 var boxTwo = function() {
     var y = window.scrollY;
     if (y >= 550) {
@@ -52,11 +22,8 @@ var boxTwo = function() {
 };
 window.addEventListener("scroll", boxTwo);
 
-
-
 // box 3
 showBoxThree = window.document.getElementById("box-3");
-
 var boxThree = function() {
     var y = window.scrollY;
     if (y >= 650) {
@@ -66,10 +33,6 @@ var boxThree = function() {
     }
 };
 window.addEventListener("scroll", boxThree);
-
-
-
-
 
 
 // skills section
@@ -97,8 +60,3 @@ var hShow = function() {
     }
 };
 window.addEventListener("scroll", hShow);
-
-
-
-
-
