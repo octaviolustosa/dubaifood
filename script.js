c2c075c19df5c33243886a31d4cfb2d9c5daeb0b
octaviolustosa <94326968+octaviolustosa@users.noreply.github.com>
2021-11-25 18:17:20 -0300

    Fixing Issues

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 showBoxOne = window.document.getElementById("box-1");
 var boxOne = function() {
     var y = window.scrollY;
-    if (y >= 350) {
+    if (y >= 375) {
         showBoxOne.className = "box-1 show"
     } else {
         showBoxOne.className = "box-1 hide"
@@ -14,7 +14,7 @@ window.addEventListener("scroll", boxOne);
 showBoxTwo = window.document.getElementById("box-2");
 var boxTwo = function() {
     var y = window.scrollY;
-    if (y >= 550) {
+    if (y >= 575) {
         showBoxTwo.className = "box-2 show-left"
     } else {
         showBoxTwo.className = "box-2 hide-left"
@@ -26,7 +26,7 @@ window.addEventListener("scroll", boxTwo);
 showBoxThree = window.document.getElementById("box-3");
 var boxThree = function() {
     var y = window.scrollY;
-    if (y >= 650) {
+    if (y >= 775) {
         showBoxThree.className = "box-3 show"
     } else {
         showBoxThree.className = "box-3 hide"
@@ -34,26 +34,24 @@ var boxThree = function() {
 };
 window.addEventListener("scroll", boxThree);
 
-
-// skills section
-
-// headline h-box
-hBoxInfo = document.getElementById('h-box-info');
-var hShow = function() {
+// box 7
+showBoxSeven = window.document.getElementById("box-7");
+var boxSeven = function() {
     var y = window.scrollY;
-    if (y >= 2500) {
-        hBoxInfo.className = 'h-box-info show'
+    if (y >= 975) {
+        showBoxSeven.className = "box-7 show-left"
     } else {
-        hBoxInfo.className = 'h-box-info hideLeft'
+        showBoxSeven.className = "box-7 hide-left"
     }
 };
-window.addEventListener("scroll", hShow);
+window.addEventListener("scroll", boxSeven);
+
 
 // h-box 
 hBox = document.getElementById('h-box');
 var hShow = function() {
     var y = window.scrollY;
-    if (y >= 2350) {
+    if (y >= 2750) {
         hBox.className = 'h-box show'
     } else {
         hBox.className = 'h-box h-hide'
