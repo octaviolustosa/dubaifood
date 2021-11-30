24a4c81343b5345beb409c2ccb513c8472fac40d
octaviolustosa <94326968+octaviolustosa@users.noreply.github.com>
2021-11-30 15:37:51 -0300

    Fixing Issues

--- script.js
+++ script.js
@@ -1,60 +1,59 @@
-// box 1
-showBoxOne = window.document.getElementById("box-1");
-var boxOne = function() {
-    var y = window.scrollY;
-    if (y >= 375) {
-        showBoxOne.className = "box-1 show"
-    } else {
-        showBoxOne.className = "box-1 hide"
-    }
-};
-window.addEventListener("scroll", boxOne);
-
-// box 2
-showBoxTwo = window.document.getElementById("box-2");
-var boxTwo = function() {
-    var y = window.scrollY;
-    if (y >= 575) {
-        showBoxTwo.className = "box-2 show-left"
-    } else {
-        showBoxTwo.className = "box-2 hide-left"
-    }
-};
-window.addEventListener("scroll", boxTwo);
-
-// box 3
-showBoxThree = window.document.getElementById("box-3");
-var boxThree = function() {
-    var y = window.scrollY;
-    if (y >= 775) {
-        showBoxThree.className = "box-3 show"
-    } else {
-        showBoxThree.className = "box-3 hide"
-    }
-};
-window.addEventListener("scroll", boxThree);
-
-// box 7
-showBoxSeven = window.document.getElementById("box-7");
-var boxSeven = function() {
-    var y = window.scrollY;
-    if (y >= 975) {
-        showBoxSeven.className = "box-7 show-left"
-    } else {
-        showBoxSeven.className = "box-7 hide-left"
-    }
-};
-window.addEventListener("scroll", boxSeven);
-
-
 // h-box 
 hBox = document.getElementById('h-box');
 var hShow = function() {
     var y = window.scrollY;
     if (y >= 2750) {
-        hBox.className = 'h-box show'
+        hBox.className = 'h-box h-show'
     } else {
         hBox.className = 'h-box h-hide'
     }
 };
 window.addEventListener("scroll", hShow);
+
+
+// slide box section
+const images = document.querySelectorAll(".static");
+
+const skills = document.querySelectorAll(".h-skills");
+
+let windowHeight = window.innerHeight
+
+function animateImages() {
+    images.forEach((image) => {
+        let bounding = image.getBoundingClientRect();
+        if (bounding.bottom > windowHeight) {
+            image.classList.add("hide");
+        } else if (bounding.top < 50) {
+            image.classList.add("hide");
+        } else {
+            image.classList.remove("hide");
+            image.classList.remove("show");
+        }
+    });
+}
+
+function animateSkills() {
+    skills.forEach((image) => {
+        let bounding = image.getBoundingClientRect();
+        if (bounding.bottom > 500) {
+            image.classList.add("h-hide");
+        } else if (bounding.top < 500) {
+            image.classList.add("h-hide");
+        } else {
+            image.classList.remove("h-hide");
+            image.classList.remove("h-show");
+        }   
+    });
+}
+
+document.addEventListener("scroll", function () {
+    animateImages();
+    document.removeEventListener("scroll", this);
+});
+
+window.addEventListener("resize", function () {
+    windowHeight = window.windowHeight;
+    window.removeEventListener("resize", this);
+});
+
+animateImages();
