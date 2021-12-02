cf02c694ecc81a3e2a41ae310097a1e37cc44e69
octaviolustosa <94326968+octaviolustosa@users.noreply.github.com>
2021-12-02 10:23:05 -0300

    Fixing Issues

--- script.js
+++ script.js
@@ -13,18 +13,19 @@ window.addEventListener("scroll", hShow);
 
 // slide box section
 const images = document.querySelectorAll(".static");
+const project = document.querySelectorAll(".static-2")
 
 const skills = document.querySelectorAll(".h-skills");
 
 let windowHeight = window.innerHeight
-
+// box section
 function animateImages() {
     images.forEach((image) => {
         let bounding = image.getBoundingClientRect();
         if (bounding.bottom > windowHeight) {
             image.classList.add("hide");
         } else if (bounding.top < 50) {
-            image.classList.add("hide");
+            image.classList.add("show");
         } else {
             image.classList.remove("hide");
             image.classList.remove("show");
